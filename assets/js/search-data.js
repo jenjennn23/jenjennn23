67b40e2e87d6eb42ba142a47b7692189ed40796f
jenjennn23 <jenniferpark323@gmail.com>
2025-08-07 05:03:11 +0000

    Deploying to gh-pages from @ jenjennn23/jenjennn23.github.io@e4a5b58d077dae89ee47fdaba84d66762372d3b0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-i-am-officially-headed-to-uc-berkeley-this-fall-to-begin-my-phd-in-cognitive-psychology-working-with-dr-mariam-aly-it-s-been-a-long-time-dream-of-mine-to-go-to-grad-school-and-become-a-cognitive-neuroscientist-one-day-so-i-m-thrilled-to-begin-this-next-chapter-at-my-top-choice-program-and-lab-bear-thankful-to-my-village-at-stanford-my-family-and-friends-for-supporting-my-journey-to-grad-school",
+          title: 'I am officially headed to UC Berkeley this Fall to begin my PhD...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
